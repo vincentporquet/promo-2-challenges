3e5a192f89b1a9997f2fd7368d24e032f5e33928
Vincent Porquet <vincent_porquet@outlook.com>
2014-04-09 10:47:06 +0200

    simple looping

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,11 +1,21 @@
 def sum_with_while(min,max)
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a while..end structure
+x = 0
+while x < 100
+  x += 1
+  puts x
+end
 end
 
 def sum_with_for(min,max)
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a for..end structure
+  x = 0
+  for x in 0..100
+    x += 1
+    puts x
+end
 end
 
 def sum_recursive(min,max)
