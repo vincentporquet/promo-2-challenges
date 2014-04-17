628c6e7f5201942261aeb2bc119edd6745334553
Vincent Porquet <vincent_porquet@outlook.com>
2014-04-17 18:01:53 +0200

    jbuvg

--- 02-OOP/05-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/05-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,29 @@
 class Dessert
+  attr_accessor :name, :calories, :flavor
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    calories < 200 ? true : false
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+  attr_accessor :flavor
+
   def initialize(name, calories, flavor)
+    @flavor = flavor
+    super(name, calories)
+  end
+
+  def delicious?
+    @flavor == "black licorice" ? false : true
   end
 end
