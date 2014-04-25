981f4fc5d59d767f0ce35a4eb1ea4c27e9e96f65
Vincent Porquet <vincent_porquet@outlook.com>
2014-04-25 12:09:20 +0200

    exo 3

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -5,17 +5,21 @@ DATABASE_PATH = "db/jukebox.sqlite"
 db = SQLite3::Database.new(DATABASE_PATH)
 
 def number_of_rows(db, table_name)
+  db.execute("SELECT COUNT (*) FROM table_name")
   #TODO: count number of rows in table table_name
 end
 
 def sorted_artists(db)
-  #TODO: return array of artists' names sorted alphabetically 
+  db.execute("SELECT name FROM Artist ORDER BY name")
+  #TODO: return array of artists' names sorted alphabetically
 end
 
 def love_tracks(db)
+  db.execute("SELECT name FROM Track WHERE name like '%love'% ")
   #TODO: return array of love songs
 end
 
 def long_tracks(db, min_length)
+  db.execute("SELECT name FROM Track WHERE Milliseconds > (#{min_length})")
   #TODO: return tracks verifying: duration > min_length (minutes)
 end
