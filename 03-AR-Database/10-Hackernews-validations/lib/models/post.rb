b8ba56aee8671389076fa5ca286cc1a6ab2ced62
Vincent Porquet <vincent_porquet@outlook.com>
2014-04-29 16:18:25 +0200

    10

--- 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, :date, :source_url, :rating, :user, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { only_integer: true }
+
 end
--- 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 end
